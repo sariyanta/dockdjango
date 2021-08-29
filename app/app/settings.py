6bb5b3bc30e8a6efd3fc3e1fac985c165b342552
sariyanta <desar@sariyanta.com>
2021-08-29 20:04:16 +0200

    Change DB to Postgress

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (bool(int(os.environ.get('DEBUG', 0))))
 
 ALLOWED_HOSTS = []
-EXTEND_ALLOWED_HOSTS = 
+ALLOWED_HOSTS.extend(
+    filter (
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
@@ -76,8 +81,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'    : 'django.db.backends.postgresql',
+        'NAME'      : os.environ.get('DB_NAME'),
+        'HOST'      : os.environ.get('DB_HOST'),
+        'USER'      : os.environ.get('DB_USER'),
+        'PASSWORD'  : os.environ.get('DB_PASS')
     }
 }
 
@@ -118,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/assets/'
+MEDIA_URL = '/static/media'
+
+STATIC_ROOT = '/vol/web/assets'
+MEDIA_ROOT = '/vol/web/media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
